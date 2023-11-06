790ffd4ac9665dc055ed38627c00fd97fb118147
Rauhul Varma <rauhul@users.noreply.github.com>
2023-11-06 11:03:05 -0800

    Add additional FileCheck tests (#26)
    Adds tests which validates out-of-bit-bounds sets from a raw view
    statically results in a trap.
    
    Splits main method into methods for each bitwidth to make ir easier to
    read.

--- Tests/MMIOFileCheckTests/Tests/TestRegisterRead.swift
+++ Tests/MMIOFileCheckTests/Tests/TestRegisterRead.swift
@@ -12,71 +12,57 @@
 import MMIO
 
 @Register(bitWidth: 8)
-struct S8 {
+struct R8 {
   @ReadWrite(bits: 0..<1)
   var lo: LO
   @ReadWrite(bits: 7..<8)
   var hi: HI
 }
-let s8 = Register<S8>(unsafeAddress: 0x1000)
+let r8 = Register<R8>(unsafeAddress: 0x1000)
 
 @Register(bitWidth: 16)
-struct S16 {
+struct R16 {
   @ReadWrite(bits: 0..<1)
   var lo: LO
   @ReadWrite(bits: 15..<16)
   var hi: HI
 }
-let s16 = Register<S16>(unsafeAddress: 0x1000)
+let r16 = Register<R16>(unsafeAddress: 0x1000)
 
 @Register(bitWidth: 32)
-struct S32 {
+struct R32 {
   @ReadWrite(bits: 0..<1)
   var lo: LO
   @ReadWrite(bits: 31..<32)
   var hi: HI
 }
-let s32 = Register<S32>(unsafeAddress: 0x1000)
+let r32 = Register<R32>(unsafeAddress: 0x1000)
 
 @Register(bitWidth: 64)
-struct S64 {
+struct R64 {
   @ReadWrite(bits: 0..<1)
   var lo: LO
   @ReadWrite(bits: 63..<64)
   var hi: HI
 }
-let s64 = Register<S64>(unsafeAddress: 0x1000)
+let r64 = Register<R64>(unsafeAddress: 0x1000)
 
-public func main() {
-  s8.modify {
-    $0.lo = 1
-    $0.hi = 1
-  }
+public func main8() {
+  _ = r8.read()
   // CHECK: %[[#REG:]] = load volatile i8
-  // CHECK-NEXT: %[[#REG+1]] = or i8 %[[#REG]], -127
-  // CHECK-NEXT: store volatile i8 %[[#REG+1]]
+}
 
-  s16.modify {
-    $0.lo = 1
-    $0.hi = 1
-  }
+public func main16() {
+  _ = r16.read()
   // CHECK: %[[#REG:]] = load volatile i16
-  // CHECK-NEXT: %[[#REG+1]] = or i16 %[[#REG]], -32767
-  // CHECK-NEXT: store volatile i16 %[[#REG+1]]
+}
 
-  s32.modify {
-    $0.lo = 1
-    $0.hi = 1
-  }
+public func main32() {
+  _ = r32.read()
   // CHECK: %[[#REG:]] = load volatile i32
-  // CHECK-NEXT: %[[#REG+1]] = or i32 %[[#REG]], -2147483647
-  // CHECK-NEXT: store volatile i32 %[[#REG+1]]
+}
 
-  s64.modify {
-    $0.lo = 1
-    $0.hi = 1
-  }
+public func main64() {
+  _ = r64.read()
   // CHECK: %[[#REG:]] = load volatile i64
-  // CHECK-NEXT: %[[#REG+1]] = or i64 %[[#REG]], -9223372036854775807
-  // CHECK-NEXT: store volatile i64 %[[#REG+1]]
 }
