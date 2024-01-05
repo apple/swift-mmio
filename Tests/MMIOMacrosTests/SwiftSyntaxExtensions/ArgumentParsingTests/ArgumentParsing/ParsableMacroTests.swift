14fd664235fb673e9fbef7bd26349ddf0d70bd14
Rauhul Varma <rauhul@apple.com>
2024-01-04 17:31:16 -0800

    Fix ParsableMacro optional argument decoding (#62)
    Fixes a bug in the ParsableMacro decoding logic that prevented parsing
    past an optional argument.

--- Tests/MMIOMacrosTests/SwiftSyntaxExtensions/ArgumentParsingTests/ArgumentParsing/ParsableMacroTests.swift
+++ Tests/MMIOMacrosTests/SwiftSyntaxExtensions/ArgumentParsingTests/ArgumentParsing/ParsableMacroTests.swift
@@ -229,7 +229,7 @@ final class ParsableMacroTests: XCTestCase {
         """,
       diagnostics: [
         .init(
-          message: ErrorDiagnostic<A>.unexpectedArgumentLabel(expected: "foo", actual: "bar").message,
+          message: ErrorDiagnostic<A>.unexpectedExtraArgument(label: "bar").message,
           line: 1,
           column: 4,
           highlight: "bar: 1")
@@ -251,15 +251,94 @@ final class ParsableMacroTests: XCTestCase {
           highlight: "baz: 1")
       ],
       macros: ["A": A.self])
+  }
 
-    // FIXME: Add test two optional arguments, only specify 2nd one
-    // struct A: ParsableMacro {
-    //   @Argument(label: "foo")
-    //   var bar: Int?
-    //   @Argument(label: "baz")
-    //   var baz: Int?
-    // }
-    // test that @A(baz: 1) works
+  func test_twoArgumentOptional_parse() {
+    struct A: MMIOArgumentParsingMacro {
+      @Argument(label: "foo")
+      var foo: Int?
+
+      @Argument(label: "bar")
+      var bar: Int?
+
+      mutating func update(
+        label: String,
+        from expression: ExprSyntax,
+        in context: MacroContext<some ParsableMacro, some MacroExpansionContext>
+      ) throws {
+        switch label {
+        case "foo":
+          try self._foo.update(from: expression, in: context)
+        case "bar":
+          try self._bar.update(from: expression, in: context)
+        default:
+          fatalError()
+        }
+      }
+    }
+
+    // Good...
+    assertMacroExpansion(
+      """
+      @A struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      macros: ["A": A.self])
+
+    assertMacroExpansion(
+      """
+      @A() struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      macros: ["A": A.self])
+
+    assertMacroExpansion(
+      """
+      @A(foo: 1) struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      macros: ["A": A.self])
+
+    assertMacroExpansion(
+      """
+      @A(bar: 1) struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      macros: ["A": A.self])
+
+    assertMacroExpansion(
+      """
+      @A(foo: 1, bar: 1) struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      macros: ["A": A.self])
+
+    // Bad...
+    assertMacroExpansion(
+      """
+      @A(foo: 1, baz: 1) struct S {}
+      """,
+      expandedSource: """
+        struct S {}
+        """,
+      diagnostics: [
+        .init(
+          message: ErrorDiagnostic<A>.unexpectedExtraArgument(label: "baz").message,
+          line: 1,
+          column: 12,
+          highlight: "baz: 1")
+      ],
+      macros: ["A": A.self])
   }
 
   func test_oneArgumentArray_parse() {
@@ -440,7 +519,7 @@ final class ParsableMacroTests: XCTestCase {
         """,
       diagnostics: [
         .init(
-          message: ErrorDiagnostic<A>.unexpectedArgumentLabel(expected: "bar", actual: "baz").message,
+          message: ErrorDiagnostic<A>.unexpectedExtraArgument(label: "baz").message,
           line: 1,
           column: 12,
           highlight: "baz: 1")
