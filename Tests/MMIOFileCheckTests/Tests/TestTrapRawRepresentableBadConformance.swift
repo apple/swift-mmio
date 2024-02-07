e5618ffd23fd717637c7ec3039c4200274e88242
Rauhul Varma <rauhul@apple.com>
2024-02-07 15:13:06 -0800

    RawRepresentable BitFieldProjectable conformance (#81)
    Adds conditional extension `RawRepresentable` types whose `RawValue` is
    a `FixedWidthInteger` to make it easier to adopt `BitFieldProjectable`.
    These types will now get default implementations for the
    `init(storage:)` and `storage(_:)` requirements and only need to provide
    an implementation for the `bitWidth: Int { get }` requirement.
    
    Conforming a `RawRepresentable` type to `BitFieldProjectable` is only
    valid if all bit representations of the raw value within the declared
    bit width are valid values.
    
    Fixes #32
    
    Co-authored-by: Sam Hastings <samhastingsis@gmail.com>

---
+++ Tests/MMIOFileCheckTests/Tests/TestTrapRawRepresentableBadConformance.swift
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift MMIO open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import MMIO
+
+// This is an invalid conformance to BitFieldProjectable used to validate
+// trapping behavior
+enum Test: UInt16, BitFieldProjectable {
+  static let bitWidth = 9
+
+  case value0 = 0x0000
+  case value1 = 0x0001
+  case value2 = 0x01FF
+}
+
+// swift-format-ignore: AlwaysUseLowerCamelCase
+//@inline(never)
+public func test_toStorage_tooSmall() {
+  // CHECK-LABEL: void @"$s4main23test_toStorage_tooSmallyyF"()
+  let value = Test.value0.storage(UInt8.self)
+  // CHECK: call void @llvm.trap()
+}
+
+// swift-format-ignore: AlwaysUseLowerCamelCase
+//@inline(never)
+public func test_fromStorage_tooSmall() {
+  // CHECK-LABEL: void @"$s4main25test_fromStorage_tooSmallyyF"()
+  let value = Test(storage: UInt8(0x0))
+  // disabled: call void @llvm.trap()
+  // disabled: llvm optimizer combines these traps for an unknown reason.
+  // CHECK: call swiftcc void @"$s4main23test_toStorage_tooSmallyyF"() #0
+}
+
+// swift-format-ignore: AlwaysUseLowerCamelCase
+//@inline(never)
+public func test_fromStorage_uninhabitedValue() {
+  // CHECK-LABEL: void @"$s4main33test_fromStorage_uninhabitedValueyyF"()
+  let value = Test(storage: UInt16(0x2))
+  // disabled: call void @llvm.trap()
+  // disabled: llvm optimizer combines these traps for an unknown reason.
+  // CHECK: call swiftcc void @"$s4main23test_toStorage_tooSmallyyF"() #0
+}
---
+++ Tests/MMIOTests/BitFieldProjectableTests.swift
@@ -0,0 +1,78 @@
+//===----------------------------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift MMIO open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+
+@testable import MMIO
+
+final class BitFieldProjectableTests: XCTestCase {
+  func test_bool_fromStorage() {
+    XCTAssertEqual(false, Bool(storage: UInt8(0x0)))
+    XCTAssertEqual(false, Bool(storage: UInt16(0x0)))
+    XCTAssertEqual(false, Bool(storage: UInt32(0x0)))
+    #if arch(x86_64) || arch(arm64)
+    XCTAssertEqual(false, Bool(storage: UInt64(0x0)))
+    #endif
+
+    XCTAssertEqual(true, Bool(storage: UInt8(0x1)))
+    XCTAssertEqual(true, Bool(storage: UInt16(0x1)))
+    XCTAssertEqual(true, Bool(storage: UInt32(0x1)))
+    #if arch(x86_64) || arch(arm64)
+    XCTAssertEqual(true, Bool(storage: UInt64(0x1)))
+    #endif
+  }
+
+  func test_bool_toStorage() {
+    XCTAssertEqual(false.storage(UInt8.self), 0x0)
+    XCTAssertEqual(false.storage(UInt16.self), 0x0)
+    XCTAssertEqual(false.storage(UInt32.self), 0x0)
+    #if arch(x86_64) || arch(arm64)
+    XCTAssertEqual(false.storage(UInt64.self), 0x0)
+    #endif
+
+    XCTAssertEqual(true.storage(UInt8.self), 0x1)
+    XCTAssertEqual(true.storage(UInt16.self), 0x1)
+    XCTAssertEqual(true.storage(UInt32.self), 0x1)
+    #if arch(x86_64) || arch(arm64)
+    XCTAssertEqual(true.storage(UInt64.self), 0x1)
+    #endif
+  }
+
+  private enum Example: UInt8, BitFieldProjectable, CaseIterable {
+    static let bitWidth = 2
+    case a = 0x0
+    case b = 0x1
+    case c = 0x3
+    case d = 0x4
+  }
+
+  func test_rawRepresentable_fromStorage() {
+    for `case` in Example.allCases {
+      XCTAssertEqual(`case`, Example(storage: UInt8(`case`.rawValue)))
+      XCTAssertEqual(`case`, Example(storage: UInt16(`case`.rawValue)))
+      XCTAssertEqual(`case`, Example(storage: UInt32(`case`.rawValue)))
+      #if arch(x86_64) || arch(arm64)
+      XCTAssertEqual(`case`, Example(storage: UInt64(`case`.rawValue)))
+      #endif
+    }
+  }
+
+  func test_rawRepresentable_toStorage() {
+    for `case` in Example.allCases {
+      XCTAssertEqual(`case`.storage(UInt8.self), UInt8(`case`.rawValue))
+      XCTAssertEqual(`case`.storage(UInt16.self), UInt16(`case`.rawValue))
+      XCTAssertEqual(`case`.storage(UInt32.self), UInt32(`case`.rawValue))
+      #if arch(x86_64) || arch(arm64)
+      XCTAssertEqual(`case`.storage(UInt64.self), UInt64(`case`.rawValue))
+      #endif
+    }
+  }
+}
