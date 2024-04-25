988695e9ccc3f04d12029ea2fba25c2aab799117
Rauhul Varma <rauhul@apple.com>
2024-04-24 17:32:34 -0700

    Decode SVD enumerated values
    Adds support for decoding enumerated values of bit fields from SVD
    files. Enumerated values are not yet leveraged by the SVD2Swift
    generator. This feature will come in a future commit.
    
    Refactors integer parsing code to reduce duplicated code. Additionally,
    add checks to avoid crashing on parsing very large integer literals
    which overflow the intended storage.
    
    Fixes #90

--- Sources/SVD/SVDEnumerationCaseDataDefault.swift
+++ Sources/SVD/SVDEnumerationCaseDataDefault.swift
@@ -15,15 +15,7 @@ import Foundation
 import FoundationXML
 #endif
 
-/// An enumeratedValue defines a map between an unsigned integer and a string.
 @XMLElement
-public struct SVDEnumerationCase {
-  /// String describing the semantics of the value. Can be displayed instead
-  /// of the value.
-  public var name: String?
-  /// Extended string describing the value.
-  public var description: String?
-  /// The value of the case or the default value for undeclared cases.
-  @XMLInlineElement
-  public var data: SVDEnumerationCaseData
+public struct SVDEnumerationCaseDataDefault {
+  var isDefault: Bool
 }
---
+++ Sources/SVD/SVDEnumerationCaseDataValue.swift
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift MMIO open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import MMIOUtilities
+
+#if canImport(FoundationXML)
+import FoundationXML
+#endif
+
+@XMLElement
+public struct SVDEnumerationCaseDataValue {
+  var value: SVDEnumeratedValueDataType
+}
+
+extension SVDEnumerationCaseDataValue {
+  public func bitPatterns() -> [UInt64] { [] }
+}
+
+/// literal format: [+]?(((0x|0X)[0-9a-fA-F]+)|([0-9]+)|((#|0b)[01xX]+))
+public struct SVDEnumeratedValueDataType {
+  var value: UInt64
+  var mask: UInt64
+}
+
+extension SVDEnumeratedValueDataType: XMLNodeInitializable {
+  init(_ node: XMLNode) throws {
+    let stringValue = try String(node)
+    var description = stringValue[...]
+    let parser = Parser<Substring, (UInt64, UInt64)>
+      .enumeratedValueDataType(UInt64.self)
+    guard
+      let value = parser.run(&description),
+      description.isEmpty
+    else { throw Errors.unknownValue(stringValue) }
+
+    self.value = value.0
+    self.mask = value.1
+  }
+}
