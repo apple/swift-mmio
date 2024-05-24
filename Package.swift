bcd1114bcb706cd013051999be41f93db9b05c7c
Rauhul Varma <rauhul@apple.com>
2024-05-24 13:09:31 -0700

    hide behind feature flag

--- Package.swift
+++ Package.swift
@@ -155,38 +155,41 @@ if featureIsEnabled(named: interposable, override: nil) {
   }
 }
 
-let symbolsURL = URL(fileURLWithPath: #file)
-  .deletingLastPathComponent()
-  .appendingPathComponent("Sources")
-  .appendingPathComponent("LLDB")
-  .appendingPathComponent("Symbols.txt")
-let symbols = try String(contentsOf: symbolsURL)
-  .split(separator: "\n")
-  .filter { !$0.hasPrefix("#") }
-let linkerFlags = symbols.flatMap { ["-Xlinker", "-U", "-Xlinker", String($0)] }
+let svd2lldb = "FEATURE_SVD2LLDB"
+if featureIsEnabled(named: svd2lldb, override: nil) {
+  let symbolsURL = URL(fileURLWithPath: #file)
+    .deletingLastPathComponent()
+    .appendingPathComponent("Sources")
+    .appendingPathComponent("LLDB")
+    .appendingPathComponent("Symbols.txt")
+  let linkerFlags = try String(contentsOf: symbolsURL)
+    .split(separator: "\n")
+    .filter { !$0.hasPrefix("#") }
+    .flatMap { ["-Xlinker", "-U", "-Xlinker", String($0)] }
 
-package.targets += [
-  .target(name: "LLDB"),
-  .target(
-    name: "SVD2LLDB",
-    dependencies: [
-      .product(name: "ArgumentParser", package: "swift-argument-parser"),
-      "LLDB",
-      "SVD",
-    ],
-    swiftSettings: [.interoperabilityMode(.Cxx)],
-    linkerSettings: [.unsafeFlags(linkerFlags)]),
-  .testTarget(
-    name: "SVD2LLDBTests",
-    dependencies: ["SVD2LLDB"],
-    swiftSettings: [.interoperabilityMode(.Cxx)]),
-]
+  package.targets += [
+    .target(name: "LLDB"),
+    .target(
+      name: "SVD2LLDB",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        "LLDB",
+        "SVD",
+      ],
+      swiftSettings: [.interoperabilityMode(.Cxx)],
+      linkerSettings: [.unsafeFlags(linkerFlags)]),
+    .testTarget(
+      name: "SVD2LLDBTests",
+      dependencies: ["SVD2LLDB"],
+      swiftSettings: [.interoperabilityMode(.Cxx)]),
+  ]
 
-package.products.append(
-  .library(
-    name: "SVD2LLDB",
-    type: .dynamic,
-    targets: ["SVD2LLDB"]))
+  package.products.append(
+    .library(
+      name: "SVD2LLDB",
+      type: .dynamic,
+      targets: ["SVD2LLDB"]))
+}
 
 // Package API Extensions
 func featureIsEnabled(named featureName: String, override: Bool?) -> Bool {
