4ff5733b3f08c5914f0818f78e6ffb466c3b3c2d
Rauhul Varma <rauhul@apple.com>
2025-09-04 12:20:36 -0700

    Reduce use of dynamic arrays
    Removes a non-constant foldable use of dynamic Arrays. This should
    hopefully open up using multi-range bit fields with Embedded Swift's
    no-allocations mode. Ideally this code would use InlineArray and Span,
    but doing so would require raising the deployment target substantially.
    I haven't fully decided if that's a good idea, so for now I'm hoping
    this change suffices.
    
    Fixes #126

--- Sources/MMIO/FixedWidthInteger+Bits.swift
+++ Sources/MMIO/FixedWidthInteger+Bits.swift
@@ -10,6 +10,8 @@
 //===----------------------------------------------------------------------===//
 
 extension FixedWidthInteger {
+  // This condition should already satisfied by the macro. This method is a
+  // soundness check used only in debug mode to catch bugs in the macro.
   @inlinable @inline(__always)
   static func bitRangeWithinBounds(bits bitRange: Range<Int>) -> Bool {
     bitRange.lowerBound >= 0 && bitRange.upperBound <= Self.bitWidth
@@ -18,14 +20,14 @@ extension FixedWidthInteger {
   @inlinable @inline(__always)
   subscript(bits bitRange: Range<Int>) -> Self {
     @inlinable @inline(__always) get {
-      precondition(Self.bitRangeWithinBounds(bits: bitRange))
+      assert(Self.bitRangeWithinBounds(bits: bitRange))
       let bitWidth = bitRange.upperBound - bitRange.lowerBound
       let bitMask: Self = 1 << bitWidth &- 1
       return (self >> bitRange.lowerBound) & bitMask
     }
 
     @inlinable @inline(__always) set {
-      precondition(Self.bitRangeWithinBounds(bits: bitRange))
+      assert(Self.bitRangeWithinBounds(bits: bitRange))
       let bitWidth = bitRange.upperBound - bitRange.lowerBound
       let bitMask: Self = 1 << bitWidth &- 1
       self &= ~(bitMask << bitRange.lowerBound)
@@ -37,29 +39,12 @@ extension FixedWidthInteger {
 
 extension FixedWidthInteger {
   @inlinable @inline(__always)
-  static func bitRangesCoalesced(bits bitRanges: [Range<Int>]) -> Bool {
-    let bitRanges = bitRanges.sorted { $0.lowerBound < $1.lowerBound }
-    var lowerBound = -1
-    for bitRange in bitRanges {
-      // Specifically ensure that the bit ranges dont overlap, e.g. the
-      // following ranges are not valid: 0..<1, 0..<2. This is to ensure ranges
-      // are coalesced before iterating reduce the number of mask and shift
-      // operations needed.
-      guard lowerBound <= bitRange.lowerBound else { return false }
-      lowerBound = bitRange.upperBound
-    }
-    return true
-  }
-
-  @inlinable @inline(__always)
   subscript(bits bitRanges: [Range<Int>]) -> Self {
     @inlinable @inline(__always) get {
-      precondition(Self.bitRangesCoalesced(bits: bitRanges))
-
       var currentShift = 0
       var value: Self = 0
       for bitRange in bitRanges {
-        precondition(Self.bitRangeWithinBounds(bits: bitRange))
+        assert(Self.bitRangeWithinBounds(bits: bitRange))
         let bitWidth = bitRange.upperBound - bitRange.lowerBound
         let bitMask: Self = 1 << bitWidth &- 1
         let valueSlice = (self >> bitRange.lowerBound) & bitMask
@@ -70,7 +55,6 @@ extension FixedWidthInteger {
     }
 
     @inlinable @inline(__always) set {
-      precondition(Self.bitRangesCoalesced(bits: bitRanges))
       var fullBitWidth = 0
       for bitRange in bitRanges {
         fullBitWidth += bitRange.upperBound - bitRange.lowerBound
@@ -80,7 +64,7 @@ extension FixedWidthInteger {
 
       var newValue = newValue
       for bitRange in bitRanges {
-        precondition(Self.bitRangeWithinBounds(bits: bitRange))
+        assert(Self.bitRangeWithinBounds(bits: bitRange))
         let bitWidth = bitRange.upperBound - bitRange.lowerBound
         let bitMask: Self = 1 << bitWidth &- 1
         self &= ~(bitMask << bitRange.lowerBound)
